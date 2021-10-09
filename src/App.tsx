168b8f6771a6f5a81c6b60f190d5c8ff846bcbaf
adawag-jef <epyadawag@gmail.com>
2021-10-09 19:41:30 +0800

    dummy route

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ import LoginPage from "./pages/login";
 import PermissionPage from "./pages/permission";
 import RegisterPage from "./pages/register";
 
+const Dummy = () => {
+  return <h1>Dummy Cmpt</h1>;
+};
+
 const App: React.FC = (props) => {
   return (
     <Loader>
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/test" component={Dummy} />
+
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/permissions" component={PermissionPage} />
