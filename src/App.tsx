6f6938bddf91448439ef37c95611fee32d1feb23
adawag-jef <epyadawag@gmail.com>
2021-10-09 20:34:49 +0800

    chore: remove dummy path

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// import Loader from "./features/loader/Loader";
+import Loader from "./features/loader/Loader";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
 import PermissionPage from "./pages/permission";
 import RegisterPage from "./pages/register";
 
-const Dummy = () => {
-  return <h1>Dummy Cmpt</h1>;
-};
-
 const App: React.FC = (props) => {
   return (
-    // <Loader>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/test" component={Dummy} />
-
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/permissions" component={PermissionPage} />
-      </Switch>
-    </Router>
-    // </Loader>
+    <Loader>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/permissions" component={PermissionPage} />
+        </Switch>
+      </Router>
+    </Loader>
   );
 };
 
